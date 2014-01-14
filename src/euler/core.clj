5b6bfa0bb581b755b0702027fcc10b97ab2f28cd
Joseph Turner <turner@miserware.com>
2014-01-14 18:14:17 -0500

    Couple more

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -11,7 +11,54 @@
         (for [i (range 1000) :when (is-mult? i)] i)
     ))
 
+(def lazy-fib
+    (map first
+        (iterate
+            (fn [[a b]] [b (+ a b)]) [0N 1N])))
+(defn problem-2
+    []
+    ; old way
+    ;(defn fib
+    ;    [a b max]
+    ;    (if (< b max)
+    ;        (conj (fib b (+ a b) max) b)
+    ;        nil
+    ;    )
+    ;)
+    ; (apply + (filter even? (fib 0 1 4000000))))
+
+    ; new hotness
+    (apply + (filter even? (take-while #(< % 4000000) lazy-fib))))
+
+(defn problem-3
+    []
+    (defn digits
+        [n]
+        (count (str n)))
+    (first (first (drop-while
+                #(< (digits (second %)) 1000)
+                (map vector (range) lazy-fib)))))
+
+(defn divisor?
+    [c n]
+    (if (== c 1)
+        false
+        (= 0 (mod n c))))
+(defn problem-4
+    []
+    (defn naive-factor
+        [n]
+        (if (= n 1)
+            nil
+            (let [factor (first (drop-while #(not (divisor? % n)) (drop-while #(< % 2) (range))))]
+                (conj (naive-factor (/ n factor)) factor))))
+    (apply max (naive-factor 600851475143)))
+        
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Problem 1:" (problem-1)))
+  (println "Problem 1:" (time (problem-1)))
+  (println "Problem 2:" (time (problem-2)))
+  (println "Problem 3:" (time (problem-3)))
+  (println "Problem 4:" (time (problem-4))))
